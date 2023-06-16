4b51da20d89631c01eb7d69db82498e627f304d4
chocolategainz <74276384+chocolategainz@users.noreply.github.com>
2023-06-16 21:54:09 +0100

    Add files via upload

---
+++ Good Foods.js
@@ -0,0 +1,6 @@
+const headerBackgroundColor = document.getElementById("colorHeader");
+colorHeader.style.backgroundColor = "#e9967a";
+//"#a0785a";
+
+const navbarBackgroundColor = document.getElementById("menuBarColor");
+navbarBackgroundColor.style.backgroundColor = "#fc89ac";
