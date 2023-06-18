737d5b230b00066dfc5b7a72f638b2c35ba88612
chocolategainz <74276384+chocolategainz@users.noreply.github.com>
2023-06-18 17:19:01 +0100

    Add files via upload

--- Good Foods.js
+++ Good Foods.js
@@ -6,3 +6,39 @@ colorHeader.style.backgroundColor = "";
 const navbarBackgroundColor = document.getElementById("menuBarColor");
 navbarBackgroundColor.style.backgroundColor = "#fc89ac";
 */
+
+
+//Reference all the filter buttons
+//Buttons
+const filterButtons = document.querySelectorAll('.filter');
+
+//List of images
+const itemList = document.querySelectorAll('.item');
+
+//Add an event listener to each button
+filterButtons.forEach((button) => {
+button.addEventListener('click' , () => {
+const filtered = button.getAttribute('data-filter'); 
+
+itemList.forEach((item) => {
+    if (filtered === 'all' || item.classList.contains(filtered)) {
+        item.style.display = 'block';
+    }else{
+        item.style.display = 'none';
+    };
+});
+
+});
+});
+
+
+
+
+
+
+
+
+
+
+
+
