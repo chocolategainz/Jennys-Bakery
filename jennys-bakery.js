e47bb628c3567c30e0b7d4fe9cab13235bf6f8aa
Aaron Thomas <thomasattraction@yahoo.com>
2024-03-12 19:07:19 +0000

    ububj

--- jennys-bakery.js
+++ jennys-bakery.js
@@ -48,7 +48,7 @@ const itemsPerPage = 1;
 
 //I am using an array of objects to store and define multiple elements under the same delaration for refrencing and iteration
 const paginationData = [
-    {image: 'Jennys-Bakery/Basque Cake.png', name: 'Basque Cake', description:'Congratulations Caitlin', date: 'May 2023'},
+    {image: './Basque Cake.png', name: 'Basque Cake', description:'Congratulations Caitlin', date: 'May 2023'},
     {image: 'submissions for competition/Victoria Spongecake.png', name: 'Victoria Spongecake', description: 'Congratulations Ben', date: 'April 2023'},
     {image: 'submissions for competition/carrot cake-Alice.png', name: 'Carrot Cake', description: 'Congratulations Alice', date: 'March 2023'},
     {image: 'submissions for competitiont/white seasame chocolate cookies-lauren.png', name: 'White Seasane Chocolate Cookies', description: 'Congratulations Lauren', date: 'Feburary 2023'},
