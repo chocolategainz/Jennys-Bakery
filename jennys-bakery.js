3a258ad1095c3f339eacad5f011f511bf30b7d4b
Aaron Thomas <thomasattraction@yahoo.com>
2024-03-12 19:17:24 +0000

    FA

--- jennys-bakery.js
+++ jennys-bakery.js
@@ -51,7 +51,7 @@ const paginationData = [
     {image: 'submissions for competition/Basque Cake.png', name: 'Basque Cake', description:'Congratulations Caitlin', date: 'May 2023'},
     {image: 'submissions for competition/Victoria Spongecake.png', name: 'Victoria Spongecake', description: 'Congratulations Ben', date: 'April 2023'},
     {image: 'submissions for competition/carrot cake-Alice.png', name: 'Carrot Cake', description: 'Congratulations Alice', date: 'March 2023'},
-    {image: 'submissions for competitiont/white seasame chocolate cookies-lauren.png', name: 'White Seasane Chocolate Cookies', description: 'Congratulations Lauren', date: 'Feburary 2023'},
+    {image: 'submissions for competition/white seasame chocolate cookies-lauren.png', name: 'White Seasane Chocolate Cookies', description: 'Congratulations Lauren', date: 'Feburary 2023'},
     {image: 'submissions for competition/Apricot & Rosemerry tart.png', name: 'Apricot & Rosemerry Tart', description: 'Congratulations Ann', date: 'Janurary 2023'},
     {image: 'submissions for competition/Brownie Cake.png', name: 'Brownie Cake', description: 'Congratulations John', date: 'December 2022'},
     {image: "submissions for competition/rainbowCake.png", name: 'Rainbow Cake', description: 'Congratulations Katie', date: 'November 2022'},
